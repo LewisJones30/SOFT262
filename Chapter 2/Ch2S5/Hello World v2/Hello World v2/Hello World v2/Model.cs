d16a7b2b1726c96324f2855c7f77f6f4f19a27b9
LewisJones30 <43572935+LewisJones30@users.noreply.github.com>
2020-02-20 10:52:12 +0000

    Section 5 Chapter 2

---
+++ Chapter 2/Ch2S5/Hello World v2/Hello World v2/Hello World v2/Model.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hello_World_v2
+{
+    class Model
+    {
+        
+
+
+    }
+}
---
+++ Chapter 2/Ch2S5/Hello World v2/Hello World v2/Hello World v2/ViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hello_World_v2
+{
+    class ViewModel
+    {
+        private Model DataModel = new Model();
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+    }
+
+
+
+}
