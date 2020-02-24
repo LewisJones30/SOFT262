62c2a312c3472f5998009a75b5496d45580f9fff
LewisJones30 <43572935+LewisJones30@users.noreply.github.com>
2020-02-24 15:28:20 +0000

    CH2S2 stuff

---
+++ Chapter 2/Ch2S2E2/Exercise2/Exercise2/Exercise2/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Exercise2
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+
+        public bool switchCase = false;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Switch_Toggled(object sender, ToggledEventArgs e)
+        {
+            if (switchCase == true)
+            {
+                switchCase = false;
+            }
+            else
+            {
+                switchCase = true;
+            }
+            if (switchCase == true)
+            {
+                messageLabel.IsVisible = true;
+            }
+            else
+            {
+                messageLabel.IsVisible = false;
+            }
+        }
+    }
+}
